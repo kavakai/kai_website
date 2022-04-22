775dfaf372cc2751a269c59a205299620e6d8291
Kai Kuller <kavakai@gmail.com>
2022-04-22 13:03:51 -0600

    Add photo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Hi, I am Kai and these are some of my projects...</h1>
       <h2>About Me =></h2>
       <p>I am a software developer by trade and a father, lover, partner, friend, adventurer, etc... by choice</p>
+      <img src="./Assets/IMG_2175.JPG" />
     </div>
   );
 }
