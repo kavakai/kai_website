35bf09c2deb6166681db6c7757dcce439f5a3b01
Kai Kuller <kavakai@gmail.com>
2022-04-22 13:07:29 -0600

    Fix photo path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import image from "./Assets/IMG_2175.JPG"
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <h1>Hi, I am Kai and these are some of my projects...</h1>
       <h2>About Me =></h2>
       <p>I am a software developer by trade and a father, lover, partner, friend, adventurer, etc... by choice</p>
-      <img src="./Assets/IMG_2175.JPG" />
+      <img src={image} />
     </div>
   );
 }
