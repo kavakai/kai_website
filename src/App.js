8267fd07dc729b6730148fda0eabc5ab7d7e07e4
Kai Kuller <kavakai@gmail.com>
2022-04-23 09:17:17 -0600

    Work on css for page

--- src/App.js
+++ src/App.js
@@ -11,10 +11,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hi, I am Kai and these are some of my projects...</h1>
-      <h2>About Me {"=>"}</h2>
-      <p>I am a software developer by trade and a father, partner, friend, adventurer, etc. by choice</p>
-      <p>I am super excited about taking my experience from my former careers in customer service and massage, and using those talents to further my evolution as a human in the ever changing field of software development. I have a strong passion for connecting with others, an easy-going attitude, and I can adapt to many situations and teams. I believe the skills I have acquired from my former careers make me an ideal member for any team/family in development. I am very eager and excited to join a diverse team looking to help change the future.</p>
+        <h1>Hi, I am Kai and these are some of my projects...</h1>
+      <header className="header">
+        <h2>About Me {"=>"}</h2>
+        <br/>
+        <p className="intro-paragraph">I am a software developer by trade and a father, partner, friend, adventurer, etc. by choice</p>
+        <br/>
+        <p className="intro-paragraph">I am super excited about taking my experience from my former careers in customer service and massage, and using those talents to further my evolution as a human in the ever changing field of software development. I have a strong passion for connecting with others, an easy-going attitude, and I can adapt to many situations and teams. I believe the skills I have acquired from my former careers make me an ideal member for any team/family in development. I am very eager and excited to join a diverse team looking to help change the future.</p>
+      </header>
       <img className="scale-in-center" src={image} />
 
       <h2>Personal Projects {"=>"}</h2>
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -5,8 +5,11 @@ export const Project = ({ title, info, liveLink, github, image, backend }) => {
   return (
     <article className="proj-card">
       <h2>{title}</h2>
+      <br/>
       <img className="scale-in-center" src={image} />
+      <br/>
       <p>{info}</p>
+      <br/>
       <a href={liveLink} target="_blank">Link to Project</a>
       <a href={github} target="_blank">Github</a>
       {backend && <a href={backend}>Server Repo</a>}
